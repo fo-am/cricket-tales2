bfcc8df74b7d4d1c27daf24982c7535ce91db4ca
dave griffiths <dave@fo.am>
2018-12-04 16:44:09 +0000

    yellow fixed: #40

--- robot/plot.py
+++ robot/plot.py
@@ -11,7 +11,7 @@ def create_plot(data,filename):
     #_, x, _ = plt.hist(data, bins=np.linspace(start, end, 100), 
     #                   histtype=u'step', normed=True)  
     x = np.arange(start, end, 0.001)
-    plt.fill_between(x, 0, density(x), lw=2, facecolor='yellow')
+    plt.fill_between(x, 0, density(x), lw=2, facecolor='#f2ff38')
     plt.axis('off')
     #plt.setp(x, linewidth=2)
     plt.savefig(filename, transparent=True)
